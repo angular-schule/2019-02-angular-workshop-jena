933866ab95e3b031b311a84531d5d662f226731c
Ferdinand Malcher <mail@ferdinand-malcher.de>
2019-02-25 15:20:19 +0100

    Dashboard, Book, ngFor

---
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'br-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  books: Book[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.books = [
+      {
+        isbn: '000',
+        title: 'Angular',
+        description: 'Grundlagen und Best Practices',
+        price: 34.90,
+        authors: ['Malcher', 'Hoppe'],
+        rating: 5
+      },
+      {
+        isbn: '111',
+        title: 'React',
+        description: 'Ein anderes Framework',
+        price: 32.90,
+        authors: ['Zeigermann', 'Hartmann'],
+        rating: 3
+      }
+    ];
+  }
+
+}
---
+++ book-rating/src/app/shared/book.ts
@@ -0,0 +1,8 @@
+export interface Book {
+    isbn: string;
+    title: string;
+    rating: number;
+    authors: string[];
+    price: number;
+    description: string;
+}
